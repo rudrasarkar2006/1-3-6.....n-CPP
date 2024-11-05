b2b4b89e2e44ace2e8aaf680ee638083c5b51014
Rudra Sarkar Surja <146901986+rudrasarkar2006@users.noreply.github.com>
2024-11-05 09:02:22 +0600

    Add files via upload

---
+++ 1,3,6,n.cpp
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+      int i,n;
+      printf("enter the value of n");
+      scanf("%d",&n);
+      for(i=3;i<=n;i+=3)
+      {
+      	printf("%d\n",i);
+	  }
+	
+getch();
+return 0;
+
+}
